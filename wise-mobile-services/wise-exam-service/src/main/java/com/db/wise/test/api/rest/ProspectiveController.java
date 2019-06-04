b122cb9cd602c798a72b6ba027fa09c0fb68cbea
narender jale <narender.jale@gmail.com>
2019-06-04 18:07:04 -0400

    removing un-used depends

--- wise-mobile-services/wise-exam-service/src/main/java/com/db/wise/test/api/rest/ProspectiveController.java
+++ wise-mobile-services/wise-exam-service/src/main/java/com/db/wise/test/api/rest/ProspectiveController.java
@@ -5,11 +5,11 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import com.db.wise.test.domain.Questions;
-import com.db.wise.test.exception.DataFormatException;
+
 import com.db.wise.test.service.ExamService;
 
 
